bbae7777a8ac916886191dceda5a5a3228de5f61
aymnn <ayman.belahsen.4@gmail.com>
2023-11-28 10:00:30 -0800

    fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for x in range(ord('a'), ord('z') + 1):
     if x != ord('q') and x != ord('e'):
-        print("(:c)".format(x), end="")
+        print("{:c}".format(x), end="")
