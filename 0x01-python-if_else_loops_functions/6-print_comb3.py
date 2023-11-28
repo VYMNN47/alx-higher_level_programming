f15b66629acbc3d29509c9a6a7840530f39afde3
aymnn <ayman.belahsen.4@gmail.com>
2023-11-28 10:29:21 -0800

    print combo again

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for x in range(10):
     for y in range(x, 10):
         if x < y:
-            print("{:d}{:d}".format(x, y), 
+            print("{:d}{:d}".format(x, y),
                     end="\n" if x == 8 and y == 9 else ", ")
