240364fb0c0cf70df0135188e980835da62be336
aymnn <ayman.belahsen.4@gmail.com>
2023-11-30 19:15:06 +0100

    calculator in python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
             print("Usage: ./100-my_calculator.py <a> <operator> <b>")
             exit(1)
     a = int(sys.argv[1])
-    b = int(sys.argv[3])
     operator = sys.argv[2]
+    b = int(sys.argv[3])
     if operator == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif operator == "-":
