860e314d93e9c786ffc195a73a853f1c9fce2de1
aymnn <ayman.belahsen.4@gmail.com>
2023-12-03 19:53:28 +0100

    highest num

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,6 @@
 def max_integer(my_list=[]):
     if len(my_list) < 1:
         return None
-    else:
-        high_num = my_list.copy()
-        high_num.sort()
-        return high_num(-1)
+    high_num = my_list.copy()
+    high_num.sort()
+    return high_num(-1)
