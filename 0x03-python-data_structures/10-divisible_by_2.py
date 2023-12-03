71f371f1fa64f6aef84460f08895182bf888e3b4
aymnn <ayman.belahsen.4@gmail.com>
2023-12-03 20:02:05 +0100

    by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -6,6 +6,8 @@ def divisible_by_2(my_list=[]):
     for x in my_list:
         if (x % 2) == 0:
             divbytwo.append(True)
+            print("{:d} is divisible by 2".format(x))
         else:
             divbytwo.append(False)
+            print("{:d} is not divisible by 2".format(x))
         return divbytwo
