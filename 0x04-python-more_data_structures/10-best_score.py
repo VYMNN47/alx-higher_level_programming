63499e593006af23181737560b324430e5df4bd4
aymnn <ayman.belahsen.4@gmail.com>
2023-12-06 23:48:54 +0100

    returns a key with the biggest integer value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    mv = 0
+    mk = None
+    for x, i in a_dictionary.items():
+        if i > mv:
+            mv = i
+            mk = x
+        return mk
