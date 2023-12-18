4f53969f98cf2bd86bcb69284a5eef3062becc19
aymnn <ayman.belahsen.4@gmail.com>
2023-12-18 21:47:29 +0100

    byte code

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    x = 0
+    for e in range(1, 3):
+        try:
+            if a is not e:
+                raise Exception('Too far')
+            x = x + a ** b / x
+        except Exception:
+            x = a + b
+            break
+    return x 
