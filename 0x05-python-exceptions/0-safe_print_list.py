bf223e835d8a209a014f73b542871eef0a51c374
aymnn <ayman.belahsen.4@gmail.com>
2023-12-18 20:53:36 +0100

    fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     y = 0
     try:
         while y < x:
-            print(my_list[y], end='')
+            print(my_list[y], end = '')
             y = y + 1
     except IndexError:
         None
