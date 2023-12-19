911107eb6ac76469ff744afc2471127a7505d4fb
aymnn <ayman.belahsen.4@gmail.com>
2023-12-19 23:34:02 +0100

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Square module."""
+
+class Square:
+    """Defines a square."""
+    def __init__(self, size=0):
+        """Constructor."""
+        @property
+        def size(self):
+            """length of the square."""
+            return self.__size
+
+        @size.setter
+        def size(self, value):
+            if not isinstance(value, int):
+                raise TypeError('size must be an integer')
+            if value < 0:
+                raise ValueError('size must be >= 0')
+            self.__size = value
+
+        def area(self):
+            """Area of this square"""
+            return self.__size ** 2
