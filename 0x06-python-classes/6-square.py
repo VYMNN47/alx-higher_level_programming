6a84f37b759e95c893d75d942ae2d518516cea7c
aymnn <ayman.belahsen.4@gmail.com>
2023-12-20 00:25:15 +0100

    fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,23 +5,26 @@ class Square:
     """Represent a square"""
 
     def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new square"""
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """comment"""
         return (self.__size)
 
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
-            raise TypeError('size must be an integer')
+            raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError('size must be >= 0')
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     @property
     def position(self):
+        """comment"""
         return (self.__position)
 
     @position.setter
@@ -29,7 +32,7 @@ class Square:
         if (not isinstance(value, tuple) or len(value) =! 2 or
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
-            raise TypeError('position must be a tuple of 2 positive integers')
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
