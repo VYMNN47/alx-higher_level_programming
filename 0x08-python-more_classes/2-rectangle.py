99f173c074216d03921307ab3a84e19279c299c5
aymnn <ayman.belahsen.4@gmail.com>
2024-01-04 20:52:44 +0100

    defines a rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -38,6 +38,11 @@ class Rectangle:
 
     def area(self):
         """returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """returns the perimeter of the rectangle"""
         return self.__width * self.__height == 0:
             return 0
         return (self.__width * 2) * (self.__height * 2)
+
