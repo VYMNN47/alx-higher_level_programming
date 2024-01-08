17754871ce3aacbcdb96a824ed35438eea5dab4f
aymnn <ayman.belahsen.4@gmail.com>
2024-01-08 21:25:27 +0100

    fix

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,15 +3,18 @@
 Contains the class MyInt
 """
 
-
 class MyInt(int):
-    def __new__(cls, *agrs, **kwargs):
-        return super(MyInt, cls).__new__(cls, *agrs, **kwargs)
+    """Switch between the = and !="""
+
+    def __new__(cls, *args, **kwargs):
+        """Create a new instance of the class"""
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
 
     def __eq__(self, other):
-        """what was != is now =="""
+        """What was != is now =="""
         return int(self) != other
 
     def __ne__(self, other):
-        """what was == is now !="""
+        """What was == is now !="""
         return int(self) == other
+
