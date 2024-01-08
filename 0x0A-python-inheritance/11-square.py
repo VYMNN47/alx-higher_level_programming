02b4dfb604678fc95dcdb7369daade347e65108f
aymnn <ayman.belahsen.4@gmail.com>
2024-01-08 21:14:01 +0100

    .

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Module for Rectangle class.'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''A subclass representing a rectangle.'''
+    def __init__(self, size):
+        '''Constructor.'''
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        '''Calculate the area.'''
+        return self.__size ** 2
+
+    def __str__(self):
+        '''Returns str representation of the square.'''
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
