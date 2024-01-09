55d2c6e5636febff4b970a7f1d2067ec067f7976
aymnn <ayman.belahsen.4@gmail.com>
2024-01-09 23:55:34 +0100

    .

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''A function that inserts a line of text to a file'''
+
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, 'r', encoding='utf-8') as file:
+        line_list = []
+        while true:
+            line = file.readline()
+            if line == "":
+                break
+            line_list.append(line)
+            if search_string in line:
+                line_list.append(new_string)
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.writelines(line_list)
