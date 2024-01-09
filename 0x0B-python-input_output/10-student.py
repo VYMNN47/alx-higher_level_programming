0ad593d84e687b25764d2db05dc19e30bdb791d7
aymnn <ayman.belahsen.4@gmail.com>
2024-01-09 23:21:36 +0100

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Contains the class Student'''
+
+class Student:
+    '''Info about the student'''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''returns a dict representation of a student instance'''
+        try:
+            for x in attrs:
+                if type(x) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        own_dict = dict()
+        for q, v in self.__dict__.items():
+            if q in attrs:
+                own_dict[q] = v
+        return own_dict
