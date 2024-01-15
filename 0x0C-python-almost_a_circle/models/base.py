aa7c741cbf3b3ac006ce0d8b4502f894c3f2d90b
aymnn <ayman.belahsen.4@gmail.com>
2024-01-15 18:54:08 +0100

    .

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -55,4 +55,13 @@ class Base:
         new.update(**dictionary)
         return new
 
+    @classmethod
+    def load_from_file(cls):
+        '''Loads str from file.'''
+        from os import path
+        file = "{}.json".format(cls.__name__)
+        if not path.isfile(file):
+            return []
+        with open(file, "r", encoding="utf-8") as f:
+            return [cls.create(**d) for d in cls.from_json_string(f.read())]
 
