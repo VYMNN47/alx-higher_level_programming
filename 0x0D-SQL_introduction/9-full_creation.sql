c628f56c515c543f138693f6dd82b3a87460e0f4
aymnn <ayman.belahsen.4@gmail.com>
2024-02-13 23:57:58 +0100

    9. Full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- a script that creates a table second_table
+create table if not exists second_table (id int, name varchar(256), score int);
+insert info second_table values (1, "John", 10)
+insert info second_table values (2, "Alex", 3)
+insert info second_table values (3, "Bob", 14)
+insert info second_table values (4, "George", 8)
