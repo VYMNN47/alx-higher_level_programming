ca954fa8e465f29d56ed41d777ca26798828a9f3
aymnn <ayman.belahsen.4@gmail.com>
2024-02-14 00:55:41 +0100

    fix

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,3 @@
 -- a script that converts hbtn_0c_0 database to UTF8
-use hbtn_0c_0
-alter table first_table
+use hbtn_0c_0 alter table first_table
 convert to character set tf8mb4 collate utf8mb4_unicode_ci;
