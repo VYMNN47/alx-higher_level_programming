bb9e84e8494803285dc581379ee5d1f00a9a6ad2
aymnn <ayman.belahsen.4@gmail.com>
2024-03-11 17:42:34 +0000

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const first_line = "C is fun";
+const second_line = "Python is cool";
+const third_line = "JavaScript is amazing";
+console.log(first_line);
+console.log(second_line);
+console.log(third_line);
