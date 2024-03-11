16cc86a9c2426c8904a860c47c7cb66d7937ed7f
aymnn <ayman.belahsen.4@gmail.com>
2024-03-11 18:00:11 +0000

    2. Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const argsLen = process.argv.length;
+
+if (argsLen === 2) {
+	console.log("No arguments");
+} else if (argsLen === 3) {
+	console.log("Arguments found");
+} else {
+	console.log("Arguments found");
+}
