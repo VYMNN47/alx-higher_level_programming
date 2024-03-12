358b1986e24b48ccc475ab65594890a46f20a7cb
aymnn <ayman.belahsen.4@gmail.com>
2024-03-12 21:54:25 +0000

    3. Rectangle #3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -9,9 +9,9 @@ class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      let s = '';
+      let e= '';
       for (let j = 0; j < this.width; j++) {
-        s += 'X';
+        e += 'X';
       }
       console.log(s);
     }
