e3b6372364b239c04dc249997f950dac9e20415c
aymnn <ayman.belahsen.4@gmail.com>
2024-03-16 22:21:31 +0000

    3. SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+"""
+
 import MySQLdb
 from sys import argv
 
