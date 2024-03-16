3254b0e4da18d3b2c96a9844df9cad98a5b194b5
aymnn <ayman.belahsen.4@gmail.com>
2024-03-16 22:11:19 +0000

    3. SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 
+#!/usr/bin/python3
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+
+This time the script is safe from
+MySQL injections!
+"""
+
 import MySQLdb
 from sys import argv
 
@@ -8,17 +19,23 @@ if __name__ == '__main__':
     Access to the database and get the states
     from the database.
     """
+
     db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
                          port=3306, db=argv[3])
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states \
-             WHERE name LIKE BINARY %(name)s \
-             ORDER BY states.id ASC", {
-             'name': argv[4]}
-             )
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                *
+            FROM
+                states
+            WHERE
+                name LIKE BINARY %(name)s
+        """, {
+            'name': argv[4]
+        })
 
-    rows = cur.fetchall()
+        rows = cur.fetchall()
 
     if rows is not None:
         for row in rows:
