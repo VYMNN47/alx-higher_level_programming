d912a7da108ea41ae6e4b627b780d1ad3ae58be5
aymnn <ayman.belahsen.4@gmail.com>
2024-03-17 15:31:27 +0000

    .

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+
 """
-lists all State objects from the database hbtn_0e_6_usa
+This script prints the State object id
+with the name passed as argument
+from the database `hbtn_0e_6_usa`.
 """
 
 from model_state import Base, State
