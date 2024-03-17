2f69aaeeb89573041e62bb0f7b45a7e1c6c91961
aymnn <ayman.belahsen.4@gmail.com>
2024-03-17 15:21:52 +0000

    8. First state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,9 +15,9 @@ if __name__ == "__main__":
     from the database.
     """
 
-    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+    db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
-    engine = create_engine(db_uri)
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
 
     session = Session()
