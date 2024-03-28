906a3038ebc0a40d0c90556cfb8a47350e1cfdbd
aymnn <ayman.belahsen.4@gmail.com>
2024-03-28 23:58:37 +0000

    .

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """a Python script that takes in a URL and an email address"""
 import sys
-import urllib.request
+import requests
 
-import sys
-import urllib.request
+
+def fetch():
+    url = sys.argv[1]
+    req = requests.get(url)
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
+
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    fetch()
