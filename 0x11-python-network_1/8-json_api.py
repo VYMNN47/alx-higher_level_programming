745fe146021d68ffd0ee088a600cffc22bec3b81
aymnn <ayman.belahsen.4@gmail.com>
2024-03-29 00:07:55 +0000

    8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """a Python script that takes in a letter and sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter """
-
+import sys
+import requests
 
 def fetch():
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
