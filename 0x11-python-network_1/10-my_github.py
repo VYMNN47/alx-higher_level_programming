0a811868b0142af9c26bd798fbb6e35f163e4fa0
aymnn <ayman.belahsen.4@gmail.com>
2024-03-29 00:13:03 +0000

    9. My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,8 +6,11 @@ import sys
 import requests
 from requests.auth import HTTPBasicAuth
 
-
-if __name__ == "__main__":
+def fecth():
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     req = requests.get("https://api.github.com/user", auth=auth)
     print(req.json().get("id"))
+
+
+if __name__ == "__main__":
+    fetch()
