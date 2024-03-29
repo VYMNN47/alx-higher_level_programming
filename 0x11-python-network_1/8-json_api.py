c41fff854ca8bc26d7159dfce9ef6e9593364642
aymnn <ayman.belahsen.4@gmail.com>
2024-03-29 00:06:56 +0000

    8. Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""a Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter """
+
+
+def fetch():
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    req = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = req.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
+
+        
+if __name__ == "__main__":
+    fetch()
