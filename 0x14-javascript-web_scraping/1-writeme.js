fdb26f90e869bb526736a1906ba09b07f63b5009
aymnn <ayman.belahsen.4@gmail.com>
2024-04-23 13:01:34 +0100

    fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,6 +5,6 @@ const fs = require('fs');
 const file = process.argv[2]
 const str = process.argv[3];
 
-fs.writeFile(file, str, 'utf-8', function(err) {
-	if (err) console.log(err);
+fs.writeFile(file, str, 'utf-8', function (err) {
+  if (err) console.log(err);
 });
