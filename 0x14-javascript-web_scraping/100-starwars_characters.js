f3b31b896366ba25ac507b839365f3cdfc680f54
aymnn <ayman.belahsen.4@gmail.com>
2024-04-23 20:49:11 +0100

    .

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+request.get(url + id, function (err, res, mv_id) {
+  if (err) {
+    console.log(err);
+  }
+  const data = JSON.parse(mv_id);
+  const cha = data.characters;
+  for (const i of cha) {
+    request.get(i, function (err, res, body1) {
+      if (err) {
+        console.log(err);
+      }
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
+    });
+  }
+});
