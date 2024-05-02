7c2b18cd1a1b7e24c5b3503e86b3863d3512becb
aymnn <ayman.belahsen.4@gmail.com>
2024-05-02 21:28:59 +0100

    fix

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,11 +1,8 @@
 $(document).ready(function () {
-  $.ajax({
-    type: 'GET',
-    url: 'https://swapi.co/api/films/?format=json',
-    success: function (data) {
-      $.each(data.results, function (i, film) {
-        $('UL#list_movies').append('<li>' + film.title + '</li>');
-      });
+  const films = 'https://swapi-api.hbtn.io/api/films/?format=json';
+  $.getJSON(films, function (response) {
+    for (const film of response.results) {
+      $('ul#list_movies').append(`<li>${film.title}</li>`);
     }
   });
 });
