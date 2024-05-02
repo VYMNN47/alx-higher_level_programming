534b20ce0efd66c0bfa130d2f81388a51a3f7b15
aymnn <ayman.belahsen.4@gmail.com>
2024-05-02 21:15:07 +0100

    fix

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,9 +1,3 @@
-$(document).ready(function () {
-  $.ajax({
-    type: 'GET',
-    url: 'https://swapi.co/api/people/5/?format=json',
-    success: function (data) {
-      $('DIV#character').text(data.name);
-    }
-  });
+$.get('https://swapi.co/api/people/5/?format=json', function (data) {
+  $('DIV#character').text(data.name);
 });
